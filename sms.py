c11fc0b7155c6c025cc3eb2bb2072d21b85ce965
Amadoose <33531149+Amadoose@users.noreply.github.com>
2020-04-13 20:33:17 -0500

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client 
  
 account_sid = 'ACd45ed68562b7fd914ad4785361c77186' 
-auth_token = '94918186d8b9e10819f5e248e18b5968' 
+auth_token = '[Auth Token]' 
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create( 
